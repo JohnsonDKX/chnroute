91606fc8db458ca14b9a37ad5310d35e13d90c9a
Johnson-DKX <1607680912@qq.com>
2021-09-01 10:49:38 +0800

    Update ros-dpbr-CT-CMCC.sh

--- ros-dpbr-CT-CMCC.sh
+++ ros-dpbr-CT-CMCC.sh
@@ -16,39 +16,24 @@ wget --no-check-certificate -c -O cernet.txt https://ispip.clang.cn/cernet_cidr.
 wget --no-check-certificate -c -O gwbn.txt https://ispip.clang.cn/gwbn_cidr.txt
 #其他
 wget --no-check-certificate -c -O other.txt https://ispip.clang.cn/othernet_cidr.txt
-
-{
-echo "/ip route rule"
-
-for net in $(cat ct.txt) ; do
-  echo "add dst-address=$net action=lookup table=CT"
-done
-
-for net in $(cat cnc.txt) ; do
-  echo "add dst-address=$net action=lookup table=CT"
-done
-
-for net in $(cat cmcc.txt) ; do
-  echo "add dst-address=$net action=lookup table=CMCC"
-done
-
-for net in $(cat crtc.txt) ; do
-  echo "add dst-address=$net action=lookup table=CMCC"
-done
-
-for net in $(cat cernet.txt) ; do
-  echo "add dst-address=$net action=lookup table=CT"
-done
-
-for net in $(cat gwbn.txt) ; do
-  echo "add dst-address=$net action=lookup table=CT"
-done
-
-for net in $(cat other.txt) ; do
-  echo "add dst-address=$net action=lookup table=CT"
-done
-} > ../ros-pbr-CT-CMCC.rsc 
-
+#中国
+wget --no-check-certificate -c -O chn.txt https://ispip.clang.cn/all_cn.txt
+#电信ipv6
+wget --no-check-certificate -c -O ct_ipv6.txt https://ispip.clang.cn/chinatelecom_ipv6.txt
+#联通ipv6
+wget --no-check-certificate -c -O cnc_ipv6.txt https://ispip.clang.cn/unicom_cnc_ipv6.txt
+#移动ipv6
+wget --no-check-certificate -c -O cmcc_ipv6.txt https://ispip.clang.cn/cmcc_ipv6.txt
+#铁通ipv6
+wget --no-check-certificate -c -O crtc_ipv6.txt https://ispip.clang.cn/crtc_ipv6.txt
+#教育网ipv6
+wget --no-check-certificate -c -O cernet_ipv6.txt https://ispip.clang.cn/cernet_ipv6.txt
+#长城宽带/鹏博士ipv6
+wget --no-check-certificate -c -O gwbn_ipv6.txt https://ispip.clang.cn/gwbn_ipv6.txt
+#其他ipv6
+wget --no-check-certificate -c -O other_ipv6.txt https://ispip.clang.cn/othernet_ipv6.txt
+#中国ipv6
+wget --no-check-certificate -c -O chn_ipv6.txt https://ispip.clang.cn/all_cn_ipv6.txt
 
 {
 echo "/ip firewall address-list"
@@ -81,6 +66,51 @@ for net in $(cat other.txt) ; do
   echo "add list=dpbr-CT address=$net"
 done
 } > ../ros-dpbr-CT-CMCC.rsc 
+
+{
+echo "/ip firewall address-list"
+
+for net in $(cat chn.txt) ; do
+  echo "add list=novpn address=$net"
+done
+} > ../ros-dpbr-CHN.rsc 
+
+{
+echo "/ip firewall address-list"
+
+for net in $(cat ct_ipv6.txt) ; do
+  echo "add list=ctcc_ipv6 address=$net"
+done
+
+for net in $(cat cnc_ipv6.txt) ; do
+  echo "add list=ctcc_ipv6 address=$net"
+done
+
+for net in $(cat cmcc_ipv6.txt) ; do
+  echo "add list=cmcc_ipv6 address=$net"
+done
+
+for net in $(cat crtc_ipv6.txt) ; do
+  echo "add list=cmcc_ipv6 address=$net"
+done
+
+for net in $(cat cernet_ipv6.txt) ; do
+  echo "add list=ctcc_ipv6 address=$net"
+done
+
+for net in $(cat gwbn_ipv6.txt) ; do
+  echo "add list=ctcc_ipv6 address=$net"
+done
+
+for net in $(cat other_ipv6.txt) ; do
+  echo "add list=ctcc_ipv6 address=$net"
+done
+} > ../ros-dpbr-CT-CMCC-IPV6.rsc 
+
+for net in $(cat chn_ipv6.txt) ; do
+  echo "add list=novpn_ipv6 address=$net"
+done
+} > ../ros-dpbr-CHN-IPV6.rsc 
 
 cd ..
 rm -rf ./pbr
